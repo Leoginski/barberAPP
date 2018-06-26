7e06f96e904ff0ea24ee888c1c86d5fe4dddfe61
leoginski <leonardo@smoginski@gmail.com>
2018-06-26 10:13:52 -0300

    Config.

--- screens/LaunchScreen.js
+++ screens/LaunchScreen.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Toast, {SHORT} from 'react-native-easy-toast'
+import axios from 'axios';
 
 import {
   Text,
@@ -15,8 +17,8 @@ export default class LaunchScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
-      password: ""
+      Username: "",
+      Password: ""
     };
   }
 
@@ -27,6 +29,42 @@ export default class LaunchScreen extends Component {
       backgroundColor: '#1e272e'
     },
   }
+  
+  handleClick(event){
+    ToastAndroid.show('Função Asincrona', ToastAndroid.SHORT);
+
+    var loginUrl = "http://thebarberwebapi.azurewebsites.net/api/Authentication/login";
+    var self = this;
+    var payload={
+    "Username":this.state.Username,
+    "Password":this.state.Password
+    }
+    axios.post(loginUrl, payload)
+    .then(function (response) {
+    console.log(response);
+    ToastAndroid.show(response, ToastAndroid.SHORT);
+
+    if(response.data.code == 200){
+
+    ToastAndroid.show('A pikachu appeared nearby !', ToastAndroid.SHORT);
+
+    navigate("Menu", { state: this.state })
+
+    // var uploadScreen=[];
+    // uploadScreen.push(<UploadScreen appContext={self.props.appContext}/>)
+    // self.props.appContext.setState({loginPage:[],uploadScreen:uploadScreen})
+    }
+    else if(response.data.code == 204){
+      ToastAndroid.show('A pikachu appeared nearby !', ToastAndroid.SHORT);
+    }
+    else{
+      ToastAndroid.show('A pikachu appeared nearby !', ToastAndroid.SHORT);
+    }
+    })
+    .catch(function (error) {
+      ToastAndroid.show(error, ToastAndroid.SHORT);
+    });
+    }
 
   render() {
     const { navigate } = this.props.navigation;
@@ -38,20 +76,21 @@ export default class LaunchScreen extends Component {
             style={styles.logo}
           />
           <Text style={styles.title}></Text>
+          <Toast ref="toast"/>
         </View>
         <View style={styles.formContainer}>
           <View style={styles.containerLoginForm}>
             <TextInput
-              value={this.state.username}
+              value={this.state.Username}
               style={styles.input}
-              onChangeText={username => this.setState({ username })}
+              onChangeText={Username => this.setState({ Username })}
               placeholder="Usuário"
               placeholderTextColor="rgba(255,255,255,0.7)"
             />
             <TextInput
-              value={this.state.password}
+              value={this.state.Password}
               style={styles.input}
-              onChangeText={password => this.setState({ password })}
+              onChangeText={Password => this.setState({ Password })}
               placeholder="Senha"
               returnKeyType="go"
               placeholderTextColor="rgba(255,255,255,0.7)"
@@ -59,7 +98,7 @@ export default class LaunchScreen extends Component {
             />
             <TouchableOpacity
               style={styles.buttonContainer}
-              onPress={() => navigate("Menu", { state: this.state })}
+              onPress={(event) => this.handleClick(event)}
             >
               <Text style={styles.buttonText}>Login</Text>
             </TouchableOpacity>
